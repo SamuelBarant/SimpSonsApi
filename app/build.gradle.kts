4e66d547a40b06ce223cdcc9d117ad06682ec00e
SamuelBarant <samucraft346@gmail.com>
2026-01-08 15:53:03 +0100

    Implements Theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.ui.text.google.fonts)
     //Compose
     val composeBom = platform("androidx.compose:compose-bom:2025.12.00")
     implementation(composeBom)
